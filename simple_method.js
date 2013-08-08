e1748cf16e5a248a202b34cb6afb50062f91a9ae
Javi <orzola@gmail.com>
2013-08-08 08:57:31 -0400

    Updated

---
+++ simple_method.js
@@ -0,0 +1,11 @@
+function Circle (radius) {
+    this.radius = radius;
+    this.area = function () {
+        return Math.PI * this.radius * this.radius;
+        
+    };
+    // define a perimeter method here
+    this.perimeter = function (){
+        return this.radius * Math.PI * 2;
+    };    
+}
