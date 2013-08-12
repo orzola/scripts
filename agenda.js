68b454b8283acd1abbefb426e25174ade7513aac
Javi <orzola@gmail.com>
2013-08-12 11:04:27 -0400

    Added agenda.js

---
+++ agenda.js
@@ -0,0 +1,49 @@
+var bob = {
+    firstName: "Bob",
+    lastName: "Jones",
+    phoneNumber: "(650) 777-7777",
+    email: "bob.jones@example.com"
+};
+
+var mary = {
+    firstName: "Mary",
+    lastName: "Johnson",
+    phoneNumber: "(650) 888-8888",
+    email: "mary.johnson@example.com"
+};
+
+var contacts = [bob, mary];
+
+function add(firstName, lastName, email, phoneNumber){
+    contacts[contacts.length] = {
+        firstName: firstName,
+        lastName: lastName,
+        phoneNumber: phoneNumber,
+        email: email
+    };
+}
+
+function printPerson(person) {
+    console.log(person.firstName + " " + person.lastName);
+}
+
+function list() {
+    var contactsLength = contacts.length;
+	for (var i = 0; i < contactsLength; i++) {
+		printPerson(contacts[i]);
+	}
+}
+
+/*Create a search function
+then call it passing "Jones"*/
+function search (lastName){
+    var contactsLength = contacts.length;
+    for (var i = 0; i < contactsLength; i++) {
+        if (lastName === contacts[i].lastName) {
+        printPerson(contacts[i]);
+        }
+    }
+}
+add("Javi", "Yeah", 8987987, "hola@hola.com");
+list();
+search("Jones");
